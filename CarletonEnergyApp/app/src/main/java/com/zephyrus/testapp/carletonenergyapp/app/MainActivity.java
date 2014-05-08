10b84d72f083fd73573d60e541f9a1245937dbf1
Michael <wheatmam@carleton.edu>
2014-05-07 20:09:40 -0500

    activity wind-updated with dividers

--- CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/MainActivity.java
+++ CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(android.R.layout.activity_main);
     }
 
 
