849769c453db9321c382538ccb45cb0450516902
katjacollier <collierk@carleton.edu>
2014-05-09 12:20:07 -0500

    Named the text box so we can use it to display data
    Added some url get data code, still very much in progress

--- DataTestApp/app/src/main/java/com/zephyrus/testapp/datatestapp/app/MainActivity.java
+++ DataTestApp/app/src/main/java/com/zephyrus/testapp/datatestapp/app/MainActivity.java
@@ -5,12 +5,43 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+import android.util.JsonReader;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.InputStreamReader;
+import java.net.URLConnection;
+
+import java.io.InputStream;
+import java.net.*;
+import java.util.Date;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Testing URL to string - should move this code back to CarletonEnergyDataSource
+        URL url = null;
+        try {
+            url = new URL("https://rest.buildingos.com/reports/timeseries/?start=2014/05/07+20:00:00&resolution=hour&end=2014/5/07+20:00:00&name=carleton_campus_en_use");
+            URLConnection urlConnection = url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            InputStreamReader reader = new InputStreamReader(in);
+            int max_size = 200000;
+            char[] buffer = new char[max_size];
+            reader.read(buffer, max_size, 0);
+            String json_string = new String(buffer);
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
