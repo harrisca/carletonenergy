596b9e216ba18cdacf11e4cebe8f64a745baf713
licha <linr@carleton.edu>
2014-05-10 15:44:50 -0500

    linked things better

--- CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/EnergyActivity.java
+++ CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/EnergyActivity.java
@@ -3,8 +3,6 @@ package com.zephyrus.testapp.carletonenergyapp.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 
@@ -14,23 +12,6 @@ public class EnergyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_energy);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.energy, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return super.onOptionsItemSelected(item);
     }
 
     public void switchWind(View view){
@@ -44,7 +25,7 @@ public class EnergyActivity extends Activity {
     }
 
     public void switchSettings(View view){
-        Intent intent = new Intent(this, GraphActivity.class);
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 
