0a2d451f2a97676ea05e890b5d3245936460d4bf
Michael <wheatmam@carleton.edu>
2014-05-10 22:04:41 -0500

    Added get current temperature

--- CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/CarletonEnergyDataSource.java
+++ CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/CarletonEnergyDataSource.java
@@ -1,14 +1,13 @@
 package com.zephyrus.testapp.carletonenergyapp.app;
 
-import android.util.JsonReader;
-import org.json.JSONObject;
-
 import java.io.BufferedInputStream;
-import java.io.InputStreamReader;
-import java.net.URLConnection;
-
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.*;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Date;
 
 /**
@@ -28,7 +27,61 @@ public class CarletonEnergyDataSource {
     }
 
     double getCurrentTemperature() {
-        return 0.0;
+        return getStantonTemperatureF();
+    }
+
+    public int getStantonTemperatureF() {
+
+        // URL oracle = new URL("w1.weather.gov/xml/current_obs/KSYN.xml");
+        URL noaa = null;
+        try {
+            noaa = new URL("http://w1.weather.gov/obhistory/KSYN.html");
+        } catch (MalformedURLException e) {
+            return -999;
+        }
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(
+                    new InputStreamReader(noaa.openStream()));
+        } catch (IOException e) {
+            return -999;
+        }
+
+        String inputLine;
+        int lineNum = 0;
+        String temp = new String();
+        try {
+            while ((inputLine = in.readLine()) != null){
+                if (lineNum == 23){
+                    temp = inputLine;
+                }
+                lineNum++;
+            }
+        } catch (IOException e) {
+            return -999;
+        }
+        try {
+            in.close();
+        } catch (IOException e) {
+            return -999;
+        }
+        char charInt;
+        String degreesF = "";
+        int end = 0;
+        String numbers = temp.substring(temp.length() - 25);
+        for (int i=0; i<numbers.length(); i++){
+            charInt=numbers.charAt(i);
+            if(charInt>=48 && charInt<=57){
+                degreesF = degreesF + charInt;
+                end = 1;
+            }
+            else{
+                if (end == 1){
+                    break;
+                }
+            }
+        }
+        return Integer.parseInt(degreesF);
     }
 
     double getCurrentWindSpeed() {
