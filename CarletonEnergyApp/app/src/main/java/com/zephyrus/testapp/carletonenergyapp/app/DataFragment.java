174d9da49b9b461083c28371332183e53135e062
harrisca <harrisca@carleton.edu>
2014-05-25 14:36:46 -0500

    Graphs are getting more functional - at least in the DataFragment

--- CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/DataFragment.java
+++ CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/DataFragment.java
@@ -2,6 +2,10 @@ package com.zephyrus.testapp.carletonenergyapp.app;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,17 +14,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.androidplot.ui.SizeLayoutType;
+import com.androidplot.ui.SizeMetrics;
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.PointLabelFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.XYStepMode;
 
+import java.text.DecimalFormat;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class DataFragment extends Fragment {
 
+    private CarletonEnergyDataSource dataSource;
     private XYPlot plot;
     private View fragView;
 
@@ -29,6 +45,8 @@ public class DataFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_data, container, false);
+
+        dataSource = new CarletonEnergyDataSource(this.getActivity());
 
         /*
         if(!isPortrait()){
@@ -41,61 +59,126 @@ public class DataFragment extends Fragment {
         // initialize our XYPlot reference:
         plot = (XYPlot) fragView.findViewById(R.id.mySimpleXYPlot);
 
-        // Create a couple arrays of y-values to plot:
-        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
-        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+        Calendar today = Calendar.getInstance();
+        Calendar yesterday = Calendar.getInstance();
+        yesterday.add(Calendar.DATE, -1);
 
-        // Turn the above arrays into XYSeries':
-        XYSeries series1 = new SimpleXYSeries(
-                Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
-                "Series1");                             // Set the display title of the series
+        String dependentVariable = "production1";
+        String increment = "hour";
 
-        // same as above
-        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+        //This will hopefully allow us to fix the scale along the x-axis
+        //right now, the axis is always in ms :/
+        Number scaleAxis;
 
-        // Create a formatter to use for drawing a series using LineAndPointRenderer
-        // and configure it from xml:
+        if(increment.equals("quarterhour")){
+            scaleAxis = 1;
+        }
+        else if (increment.equals("hour")){
+            scaleAxis = 1;
+        }
+        else scaleAxis = 1;
 
-        //Method 1 of adding a series ------
-        //LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
-        //series1Format.setPointLabelFormatter(new PointLabelFormatter(Color.WHITE));
-        //series1Format.configure(getActivity().getApplicationContext(), R.xml.line_point_formatter_with_plf1);
+        ArrayList<Double> productionGraphData = dataSource.getGraphData(dependentVariable, yesterday.getTime(), today.getTime(), increment);
+        ArrayList<Double> consumptionGraphData = dataSource.getGraphData("consumption", yesterday.getTime(), today.getTime(), "hour");
 
-        // add a new series' to the xyplot:
-        //plot.addSeries(series1, series1Format);
-        //------
+        //Converting ArrayList<Double> to Number[]
+        Number[] productionNums = new Number[productionGraphData.size()];
+        for(int i = 0; i<productionNums.length; i++){
+            productionNums[i] = i;
+            System.out.println(productionNums[i]);
+        }
 
+        Number[] consumptionNums = new Number[consumptionGraphData.size()];
+        for(int i = 0; i<consumptionNums.length; i++){
+            consumptionNums[i] = i;
+            System.out.println(consumptionNums[i]);
+        }
 
-        //Method 2 -------
-
-
-        plot.addSeries(
-                series1,
-                new LineAndPointFormatter(Color.rgb(0, 0, 200), Color.rgb(0, 0, 100),
-                null,
-                (PointLabelFormatter) null));
-
-        // same as above:
-        //LineAndPointFormatter series2Format = new LineAndPointFormatter(Color.YELLOW, Color.BLUE, Color.GREEN, null);
-        //series2Format.setPointLabelFormatter(new PointLabelFormatter(Color.BLACK));
-        //series2Format.configure(getActivity().getApplicationContext(), R.xml.line_point_formatter_with_plf2);
-        //plot.addSeries(series2, series2Format);
+        Number[] timeNums = new Number[productionGraphData.size()];
+        timeNums = (Number[]) productionGraphData.toArray(timeNums);
 
 
-        plot.addSeries(
-                series2,
-                new LineAndPointFormatter(Color.rgb(0, 0, 200), Color.rgb(0, 0, 100),
-                        null,
-                        (PointLabelFormatter) null));
+        // create our series from our array of nums:
+        XYSeries production = new SimpleXYSeries(
+                Arrays.asList(productionNums),
+                Arrays.asList(timeNums),
+                "Production");
 
+        //XYSeries consumption = new SimpleXYSeries(
+         //       Arrays.asList(consumptionNums),
+           //     Arrays.asList(timeNums),
+             //   "Consumption");
 
+        plot.getGraphWidget().getGridBackgroundPaint().setColor(Color.WHITE);
+        plot.getGraphWidget().getDomainGridLinePaint().setColor(Color.BLACK);
+        plot.getGraphWidget().getDomainGridLinePaint().
+                setPathEffect(new DashPathEffect(new float[]{1, 1}, 1));
+        plot.getGraphWidget().getRangeGridLinePaint().setColor(Color.BLACK);
+        plot.getGraphWidget().getRangeGridLinePaint().
+                setPathEffect(new DashPathEffect(new float[]{1, 1}, 1));
+        plot.getGraphWidget().getDomainOriginLinePaint().setColor(Color.BLACK);
+        plot.getGraphWidget().getRangeOriginLinePaint().setColor(Color.BLACK);
+        plot.getGraphWidget().setSize(new SizeMetrics(
+                1, SizeLayoutType.FILL,
+                10, SizeLayoutType.FILL));
 
-        // reduce the number of range labels
-        //plot.setTicksPerRangeLabel(3);
-        //plot.getGraphWidget().setDomainLabelOrientation(-45);
+        // Create a formatter to use for drawing a series using LineAndPointRenderer:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(
+                Color.rgb(0, 0, 100),                   // line color
+                Color.rgb(0, 0, 100),                   // point color
+                Color.rgb(0, 0, 200), null);                // fill color
 
+        // setup our line fill paint to be a slightly transparent gradient:
+        Paint lineFill = new Paint();
+        lineFill.setAlpha(200);
 
+        // ugly usage of LinearGradient. unfortunately there's no way to determine the actual size of
+        // a View from within onCreate.  one alternative is to specify a dimension in resources
+        // and use that accordingly.  at least then the values can be customized for the device type and orientation.
+        lineFill.setShader(new LinearGradient(0, 0, 200, 200, Color.WHITE, Color.BLUE, Shader.TileMode.CLAMP));
+
+        LineAndPointFormatter formatter  =
+                new LineAndPointFormatter(Color.rgb(0, 0,0), Color.BLUE, Color.RED, null);
+        formatter.setFillPaint(lineFill);
+        plot.getGraphWidget().setPaddingRight(2);
+
+        plot.addSeries(production, formatter);
+
+        //plot.addSeries(consumption, new LineAndPointFormatter(Color.rgb(0, 0,0), Color.RED, Color.RED, null));
+
+        // draw a domain tick for each year:
+        plot.setDomainStep(XYStepMode.SUBDIVIDE, timeNums.length);
+
+        // customize our domain/range labels
+        plot.setDomainLabel("Time");
+        plot.setRangeLabel("Power (kW)");
+
+        // get rid of decimal points in our range labels:
+        plot.setRangeValueFormat(new DecimalFormat("0"));
+
+        plot.setDomainValueFormat(new Format() {
+
+            // create a simple date format that draws on the year portion of our timestamp.
+            // see http://download.oracle.com/javase/1.4.2/docs/api/java/text/SimpleDateFormat.html
+            // for a full description of SimpleDateFormat.
+            private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy");
+
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+
+                // because our timestamps are in seconds and SimpleDateFormat expects milliseconds
+                // we multiply our timestamp by 1000:
+                long timestamp = ((Number) obj).longValue() * 1000;
+                Date date = new Date(timestamp);
+                return dateFormat.format(date, toAppendTo, pos);
+            }
+
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+
+            }
+        });
 
         return rootView;
     }
