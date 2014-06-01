9047c6c6c1be52c7fa6838ad27d6210acd86a502
harrisca <harrisca@carleton.edu>
2014-06-01 13:16:55 -0500

    Cleaned GraphActivity

--- CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/GraphActivity.java
+++ CarletonEnergyApp/app/src/main/java/com/zephyrus/testapp/carletonenergyapp/app/GraphActivity.java
@@ -27,44 +27,23 @@ import java.util.TimeZone;
 
 public class GraphActivity extends Activity {
 
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_graph);
-
-        if(!isLandscape()){
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        }
-    }
-
-    public boolean isLandscape() {
-        Display getOrient = getWindowManager().getDefaultDisplay();
-        if (getOrient.getWidth() >= getOrient.getHeight())
-            return true;
-        else
-            return false;
-    }*/
-
     private XYPlot plot;
     private CarletonEnergyDataSource dataSource;
     private static boolean productionChecked;
     private static boolean consumptionChecked;
-    private String buttonClicked;
+    private String buttonClickedStr;
     private String increment = "quarterhour";     //must be quarter-hour for day; hour for week; day for month/year
     private Calendar startTime;
     private Calendar endTime;
     private String graphTitle = "Energy Data";
 
-
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        buttonClicked = getIntent().getStringExtra("buttonClicked");
+        buttonClickedStr = getIntent().getStringExtra("buttonClicked");
         productionChecked = getIntent().getBooleanExtra("productionChecked", true);
         consumptionChecked = getIntent().getBooleanExtra("consumptionChecked", true);
-
 
         dataSource = CarletonEnergyDataSource.getSingleton();
 
@@ -76,32 +55,27 @@ public class GraphActivity extends Activity {
             finish();
         }
 
-        // fun little snippet that prevents users from taking screenshots
-        // on ICS+ devices :-)
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
-                WindowManager.LayoutParams.FLAG_SECURE);
-
         setContentView(R.layout.activity_graph);
 
         // initialize our XYPlot reference:
         plot = (XYPlot) findViewById(R.id.graphActivityPlot);
 
-
-        if(buttonClicked.equals("week")){
+        //Set start times, end times, graph title, and increment based on which button was clicked
+        if(buttonClickedStr.equals("week")){
             graphTitle = "This Week's Energy";
             endTime = Calendar.getInstance();
             startTime = Calendar.getInstance();
             startTime.add(Calendar.DATE, -7);
             increment = "hour";
         }
-        else if (buttonClicked.equals("month")){
+        else if (buttonClickedStr.equals("month")){
             graphTitle = "This Month's Energy";
             endTime = Calendar.getInstance();
             startTime = Calendar.getInstance();
             startTime.add(Calendar.MONTH, -1);
             increment = "day";
         }
-        else if (buttonClicked.equals("year")){
+        else if (buttonClickedStr.equals("year")){
             graphTitle = "This Year's Energy";
             endTime = Calendar.getInstance();
             startTime = Calendar.getInstance();
@@ -116,16 +90,14 @@ public class GraphActivity extends Activity {
             increment = "quarterhour";
         }
 
+        //Get our data from the CarletonEnergyDataSource
         ArrayList<Double> productionGraphData = dataSource.getGraphData("production1", startTime.getTime(), endTime.getTime(), increment);
         ArrayList<Double> consumptionGraphData = dataSource.getGraphData("consumption", startTime.getTime(), endTime.getTime(), increment);
-        Log.i("graph_data", productionGraphData.size() + "");
-        Log.i("graph_data", productionGraphData.size() + "");
 
         //Converting ArrayList<Double> to Number[]
         Number[] productionNums = new Number[productionGraphData.size()];
         for(int i = 0; i<productionNums.length; i++){
             productionNums[i] = productionGraphData.get(i);
-            //System.out.println(productionNums[i]);
         }
 
         Number[] consumptionNums = new Number[consumptionGraphData.size()];
@@ -134,9 +106,8 @@ public class GraphActivity extends Activity {
         }
 
         Number[] timeNums = new Number[productionGraphData.size()];
-        //timeNums = (Number[]) productionGraphData.toArray(timeNums);
-        for (int i = 0; i < timeNums.length; i++) {
 
+        for (int i = 0; i < timeNums.length; i++) {
             // calculates the number of milliseconds that passed between consecutive data points
             long increment_ms = 0l;
             if (increment.equals("quarterhour")) {
@@ -151,54 +122,33 @@ public class GraphActivity extends Activity {
             timeNums[i] = i * increment_ms + startTime.getTime().getTime();
         }
 
-        // create our series from our array of nums:
+        // create our production series from our array of nums:
         XYSeries production = new SimpleXYSeries(
                 Arrays.asList(timeNums),
                 Arrays.asList(productionNums),
                 "Production");
 
+        // create our consumption series from our array of nums:
         XYSeries consumption = new SimpleXYSeries(
                Arrays.asList(timeNums),
                Arrays.asList(consumptionNums),
                "Consumption");
 
-        ///plot.getGraphWidget().getGridBackgroundPaint().setColor(Color.WHITE);
-        ///plot.getGraphWidget().getDomainGridLinePaint().setColor(Color.BLACK);
-        //plot.getGraphWidget().getDomainGridLinePaint().
-          //      setPathEffect(new DashPathEffect(new float[]{1, 1}, 1));
-        ///plot.getGraphWidget().getRangeGridLinePaint().setColor(Color.BLACK);
-        //plot.getGraphWidget().getRangeGridLinePaint().
-          //      setPathEffect(new DashPathEffect(new float[]{1, 1}, 1));
-        plot.getGraphWidget().getDomainOriginLinePaint().setColor(Color.BLACK);
-        plot.getGraphWidget().getRangeOriginLinePaint().setColor(Color.BLACK);
-        plot.getGraphWidget().setSize(new SizeMetrics(1, SizeLayoutType.FILL, 10, SizeLayoutType.FILL));
+        //Formatting our graph display
+        XYGraphWidget gw = plot.getGraphWidget();
 
-        // Create a formatter to use for drawing a series using LineAndPointRenderer:
-        /*LineAndPointFormatter series1Format = new LineAndPointFormatter(
-                Color.rgb(0, 0, 100),                   // line color
-                Color.rgb(0, 0, 0),                   // point color
-                Color.rgb(0, 0, 0), null);                // fill color
-        */
-        // setup our line fill paint to be a slightly transparent gradient:
-        //Paint lineFill = new Paint();
-        //lineFill.setAlpha(200);
-
-        // ugly usage of LinearGradient. unfortunately there's no way to determine the actual size of
-        // a View from within onCreate.  one alternative is to specify a dimension in resources
-        // and use that accordingly.  at least then the values can be customized for the device type and orientation.
-        //lineFill.setShader(new LinearGradient(0, 0, 200, 200, Color.WHITE, Color.BLUE, Shader.TileMode.CLAMP));
+        gw.getDomainOriginLinePaint().setColor(Color.BLACK);
+        gw.getRangeOriginLinePaint().setColor(Color.BLACK);
+        gw.setSize(new SizeMetrics(1, SizeLayoutType.FILL, 10, SizeLayoutType.FILL));
+        gw.setPaddingRight(2);
 
         LineAndPointFormatter proFormatter = new LineAndPointFormatter(Color.rgb(0,51,102), Color.rgb(0,51,102), null, null);
-        //formatter.setFillPaint(lineFill);
-        //proFormatter.setFillPaint(null);
-        plot.getGraphWidget().setPaddingRight(2);
+        LineAndPointFormatter conFormatter = new LineAndPointFormatter(Color.rgb(153,0,0), Color.rgb(153,0,0), null, null);
+
+        //Graph production and consumption only if they were checked
         if(productionChecked == true) {
             plot.addSeries(production, proFormatter);
         }
-
-
-        LineAndPointFormatter conFormatter = new LineAndPointFormatter(Color.rgb(153,0,0), Color.rgb(153,0,0), null, null);
-        //conFormatter.setFillPaint(null);
         if(consumptionChecked == true) {
             plot.addSeries(consumption, conFormatter);
         }
@@ -212,7 +162,8 @@ public class GraphActivity extends Activity {
         // get rid of decimal points in our range labels:
         plot.setRangeValueFormat(new DecimalFormat("0"));
 
-        if (buttonClicked.equals("day")) {
+        //Block deals with date formatting for our ticks and labels
+        if (buttonClickedStr.equals("day")) {
             plot.setDomainValueFormat(new Format() {
 
                 // create a simple date format that draws on the year portion of our timestamp.
@@ -234,13 +185,12 @@ public class GraphActivity extends Activity {
                 @Override
                 public Object parseObject(String source, ParsePosition pos) {
                     return null;
-
                 }
             });
         }
-        else if (buttonClicked.equals("month") || buttonClicked.equals("week")) {
+        else if (buttonClickedStr.equals("month") || buttonClickedStr.equals("week")) {
 
-            if (buttonClicked.equals("week")) {
+            if (buttonClickedStr.equals("week")) {
                 // draw a domain tick for each day:
                 plot.setDomainStep(XYStepMode.SUBDIVIDE, 7);
             }
@@ -251,8 +201,6 @@ public class GraphActivity extends Activity {
                 // see http://download.oracle.com/javase/1.4.2/docs/api/java/text/SimpleDateFormat.html
                 // for a full description of SimpleDateFormat.
                 private SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd");
-
-
 
                 @Override
                 public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
@@ -268,11 +216,10 @@ public class GraphActivity extends Activity {
                 @Override
                 public Object parseObject(String source, ParsePosition pos) {
                     return null;
-
                 }
             });
         }
-        else if (buttonClicked.equals("year")) {
+        else if (buttonClickedStr.equals("year")) {
             // show tick for each month
             plot.setDomainStep(XYStepMode.SUBDIVIDE, 12);
             plot.setDomainValueFormat(new Format() {
@@ -301,10 +248,9 @@ public class GraphActivity extends Activity {
                 }
             });
         }
-
-
     }
 
+    //Returns true if the device is in landscape
     public boolean isLandscape() {
         Display getOrient = getWindowManager().getDefaultDisplay();
         if (getOrient.getWidth() >= getOrient.getHeight())
